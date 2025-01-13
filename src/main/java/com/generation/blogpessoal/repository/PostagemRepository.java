64405cac47b4f838bb62b2252c78f1f6cfe4011c
Rosifaarias <rosilene.farias00@gmail.com>
2025-01-13 17:07:50 -0300

    crud

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,28 +1,12 @@
 package com.generation.blogpessoal.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-
-// SAVE(OBJETO OBJETO)
-	
-	// FINDBYID (LONG ID)
-	
-	// EXISTBYID (LONG ID)
-	
-	// FINDALL
-	// DELETEBYID (LONG ID)
-	// DELETEALL ()
-
-
-
-
-
-
-
-
-
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
