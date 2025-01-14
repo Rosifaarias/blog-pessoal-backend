1a0cdbee13e661d4c9e65445a4585bc40d2d3d6c
Rosifaarias <rosilene.farias00@gmail.com>
2025-01-14 17:10:44 -0300

    CRIANDO O TEMA

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,21 +4,24 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-@Entity  // etiquetas para entender os comportamentos de classe/att e metodos
+@Entity // etiquetas para entender os comportamentos de classe/att e metodos
 @Table(name = "tb_postagens") // indica o nome da tabela no banco de dados
 public class Postagem {
 	@Id // ELE É A CHAVE PRIMARIA DESSA TABELA
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO INCREMENTO
-	private Long id; 
-	
+	private Long id;
+
 	@NotBlank(message = "Esse campo é obrigatório") // not null TITULO DA POSTAGEM
 	@Size(min = 5, max = 100, message = "Digite no minino 5 e no maximo 100 caracteres")
 	private String titulo;
@@ -26,35 +29,55 @@ public class Postagem {
 	@NotBlank(message = "Esse campo é obrigatório") // not null TITULO DA POSTAGEM
 	@Size(min = 10, max = 1000, message = "Digite no minino 5 e no maximo 100 caracteres")
 	private String texto;
-	
+
 	@UpdateTimestamp
 	private LocalDateTime data;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema; // conectando a postagem
 	
+
 	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public LocalDateTime getData() {
 		return data;
 	}
+
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
-	
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 }
